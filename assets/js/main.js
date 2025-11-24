0db559f6710a6816644f8fcc3c2fa1196afd7716
AukaHope <ratchapol.toop@mail.kmutt.ac.th>
2025-11-24 20:16:43 +0700

    fix:[index.php,main.css],put:[main.js]

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,21 @@ window.addEventListener("load", function () {
     overlay.remove();
   });
 });
+
+// Show password
+document.addEventListener("DOMContentLoaded", function () {
+  let eyeicon = document.getElementById("eyeicon");
+  let password = document.getElementById("password-toggle");
+
+  eyeicon.onclick = function () {
+    if (password.type === "password") {
+      password.type = "text";
+      eyeicon.classList.remove("fa-eye-slash");
+      eyeicon.classList.add("fa-eye");
+    } else {
+      password.type = "password";
+      eyeicon.classList.remove("fa-eye");
+      eyeicon.classList.add("fa-eye-slash");
+    }
+  };
+});
--- index.php
+++ index.php
@@ -13,37 +13,83 @@ require_once __DIR__ . '/src/Services/auth/login.php';
 
         <div class="container-login-section">
 
-            <div class="header-login">
+            <header class="header-login">
                 <div class="logo-login">
-                    <img src="assets\img\favicon\deebuk-logo.png" alt="DB-logo">
+                    <img src="assets/img/favicon/deebuk-logo.png" alt="DB-logo">
                 </div>
                 <div class="text-header-login">
                     <h3>ระบบสำนักงานอิเล็กทรอนิกส์</h3>
                     <h3>โรงเรียนดีบุกพังงาวิทยายน</h3>
                 </div>
-            </div>
+            </header>
 
-            <div class="form-login">
+            <section class="form-login">
                 <form action="" method="post">
-                    <input type="text" name="pID" id="" placeholder="เลขบัตรประชาชน" required>
-                    <input type="password" name="password" id="" placeholder="รหัสผ่าน" required>
-                    <input type="checkbox" name="" id="">
-                    <p>จดจำฉัน</p>
-                    <div class="button-login">
-                        <input type="submit" value="เข้าสู่ระบบ">
+                    <div class="input-id-login-group">
+                        <label for="pID">เลขบัตรประชาชน</label>
+                        <input type="text" name="pID" id="" placeholder="เลขบัตรประชาชน" autocomplete="username" inputmode="numeric" required>
+                    </div>
+
+                    <div class="input-password-login-group">
+                        <label for="password">รหัสผ่าน</label>
+                        <input type="password" name="password" id="password-toggle" placeholder="รหัสผ่าน" autocomplete="current-password" required>
+                        <i class="fa-regular fa-eye-slash" id="eyeicon"></i>
+                    </div>
+
+                    <label class="remember-me-group">
+                        <label for="remember-me">จดจำฉัน</label>
+                        <input type="checkbox" name="remember-me" id="">
+                        <span class="checkmark"></span>
+                        <p>จดจำฉัน</p>
+                    </label>
+
+                    <div class="button-login-group">
+                        <button type="submit">เข้าสู่ระบบ</button>
                     </div>
                 </form>
-            </div>
+            </section>
 
-            <div class="footer-login">
+            <footer class="footer-login">
                 <p>ระบบสำนักงานอิเล็กทรอนิกส์ โรงเรียนดีบุกพังงาวิทยายน</p>
                 <p>DB HUB V.1.0.0 Copyright @T&T-2025 All rights reserved</p>
                 <p>Paperless office พ.ศ.2568</p>
-            </div>
+            </footer>
 
         </div>
 
-        <div class="container-notification-section"></div>
+        <div class="container-notification-section">
+
+            <div class="announcement-bar">
+                <i class="fa-solid fa-bullhorn"></i>
+                <p>วันนี้ นายดลยวัฒน์ สันติพิทักษ์ ผู้อำนวยการโรงเรียนดีบุกพังงาวิทยายน ปฏิบัติราชการ</p>
+            </div>
+
+            <div class="news-bar">
+                <div class="header-news-bar">
+                    <p>ข่าวประชาสัมพันธ์</p>
+                    <a href="#">ดูข่าวทั้งหมด</a>
+                </div>
+                
+                <div class="details-news-bar">
+                    <ul>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                        <li><p>การดำเนินการสอบแก้ตัว ภาคเรียนที่ 1 ปีการศึกษา 2568</p></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="container-calendar">
+
+            </div>
+
+        </div>
 
     </div>
 
