020d1c013f9807d99aba58a35c92b4c82f5fe2b4
AukaHope <ratchapol.toop@mail.kmutt.ac.th>
2026-01-15 23:52:40 +0700

    test

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ require_once __DIR__ . '/src/Services/system/exec-duty-announcement.php';
                         <label for="password">รหัสผ่าน</label>
                         <input type="password" name="password" id="password-toggle" placeholder="รหัสผ่าน" required>
                     </div>
-
+                    
                     <label class="remember-me-group">
                         <input type="checkbox" name="remember-me" id="remember">
                         <span class="checkmark"></span>
