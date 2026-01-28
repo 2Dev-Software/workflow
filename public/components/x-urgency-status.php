e2c693436f2950e645f17bf582ad6cd701d7a7e6
AukaHope <ratchapol.toop@mail.kmutt.ac.th>
2026-01-28 23:51:17 +0700

    FE012

---
+++ public/components/x-urgency-status.php
@@ -0,0 +1,21 @@
+<!-- --color-success -->
+<!-- outline : #00AE2C -->
+<!-- <button class="urgency-status normal">ปกติ</button> -->
+
+
+
+<!-- --color-purple -->
+<!-- outline : #9A00AF -->
+<!-- <button class="urgency-status urgen">ด่วน</button> -->
+
+
+
+<!-- --color-orange -->
+<!-- outline : #CE6203 -->
+<!-- <button class="urgency-status very-urgen">ด่วนมาก</button> -->
+
+
+
+<!-- --color-danger -->
+<!-- outline : #BD0000 -->
+<!-- <button class="urgency-status extremly-urgen">ด่วนที่สุด</button> -->
