e9dd02b5e7158a5a951fb19dbd4157b2a4f6dbfd
ParkPawapon <pawaponthammalangka@outlook.com>
2026-04-18 16:39:41 +0700

    fix: update footer branding text

--- public/components/partials/x-footer.php
+++ public/components/partials/x-footer.php
@@ -1,4 +1,3 @@
 <footer class="footer">
-    <p>ระบบงานสารบรรณออนไลน์ โรงเรียนดีบุกพังงาวิทยายน</p>
-    <p>DB SARABUN V.1.0.0 Copyright © 2026 TPH. All rights reserved</p>
+    <p>ระบบงานสารบรรณออนไลน์ โรงเรียนดีบุกพังงาวิทยายน DB SARABUN V.1.0.0 Copyright © 2026 TPH. All rights reserved</p>
 </footer>
